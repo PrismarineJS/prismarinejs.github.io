54a4f106309a4a89b3a96bd921e593941e9d1eb5
mhsjlw <mhsjlw@users.noreply.github.com>
2018-05-20 11:19:15 -0400

    Release React version

--- js/index.js
+++
@@ -1,6 +0,0 @@
-$(document).on('mousemove', function(e) {
-	var posX = e.clientX / 20;
-	var posY = e.clientY / 20;
-	var clientPosition = `${posX}px ${posY}px, 0px 0px`;
-	$('.promoBanner').css('background-position', clientPosition);
-});
